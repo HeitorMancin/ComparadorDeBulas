6382c75470907bed0ba4fe745c77eb9fdb195c71
Heitor Mancini <159497313+HeitorMancin@users.noreply.github.com>
2025-02-19 08:40:50 -0300

    Update Comparador.py

--- Comparador.py
+++ Comparador.py
@@ -147,11 +147,11 @@ if uploaded_files:
             system_instruction=( 
                     """Responda sempre em formato de tabela HTML pronto para renderização sem o ```html, comparando dois medicamentos fornecidos. A tabela deve incluir as 4 colunas seguintes:
                     Item: A seção da bula sendo comparada.
-                    Nome do Medicamento 1 (Resumo): Um breve resumo do primeiro medicamento e página onde se encontra (ex: Depakene).
-                    Nome do Medicamento 2 (Resumo): Um breve resumo do segundo medicamento e página onde se encontra (ex: Epilenil).
+                    Nome do Medicamento 1 (Resumo): Um breve resumo do primeiro medicamento (ex: Depakene).
+                    Nome do Medicamento 2 (Resumo): Um breve resumo do segundo medicamento (ex: Epilenil).
                     Diferença: Uma descrição concisa das diferenças entre os medicamentos.
                     Instruções Adicionais:
-                    Compare todas as seções da bula de ambos os medicamentos.
+                    Compare todas as seções da bula de ambos os medicamentos e dica em cada resumo de cada seção, a página onde se encontra o conteúdo.
                     Ignore diferenças de formatação, focando apenas no conteúdo.
                     **Procure nas bulas se contém imagens, e somente caso contiver, inclua a seção 'Imagens' na tabela e fale de maneira bem detalhada a descrição e as diferenças das imagens.**
                     Faça em último lugar uma seção sobre os dizeres legais.
